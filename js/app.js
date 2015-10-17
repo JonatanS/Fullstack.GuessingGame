8acb888aa0a659889e1a8b6d5970e931c65ffefa
jonatans <jonatan@jschumacher.com>
2015-10-17 12:56:14 -0400

    planned out JS file

---
+++ js/app.js
@@ -0,0 +1,41 @@
+/*app.js
+
+by Jonatan Schumacher
+10/17/2015
+
+*/
+
+//global variables
+//arrGuesses[]
+//randomly generated number 'targetNumber'
+
+//newGame function:
+	//generate random number
+	//reset array of guesses
+	//enable play button
+	//enable eye button
+	//reset 5 hearts
+
+//submitGuess function:
+	//compare guess to targetNumber
+	//if === targetNumber: playerWins function
+	//else:
+		//add guess to arrGuesses[]
+		//playerFeedback function
+
+
+//playerFeedback function:
+	//evaluate whether too high or too low
+	//evaluate whether guess is warm or cold
+
+	//modify UI:
+		//paint up or down arrow in correct color
+		//change canvas background
+
+//viewResult button:
+
+
+//playerWins function:
+	//disable play button
+	//disable eye button
+
